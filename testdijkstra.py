64bbb348f151f08b1ca39a903102b67e36d791a1
ktjosh <ksj4205@rit.edu>
2018-11-02 19:49:38 -0400

    Dump commit: new test case for testing

--- testdijkstra.py
+++ testdijkstra.py
@@ -4,19 +4,45 @@ from Priority_queue import *
 
 def create_graph():
     g= simple_graph()
-    # nodes = [1,2,3,4,5]
-    # edges = [(1,2,3),(1,3,10),(2,3,4),(2,4,7),(2,5,3),(3,4,1),(4,5,4)]
-    # nodes = [1, 2, 3, 4, 5]
-    # edges = [(1, 2, 3), (1, 3, 10), (2, 3, 5), (2, 4, 7), (2, 5, 3), (3, 4, 1),(3, 5, 1), (4, 5, 4)]
+        # nodes = [1,2,3,4,5]
+        # edges = [(1,2,3),(1,3,10),(2,3,4),(2,4,7),(2,5,3),(3,4,1),(4,5,4)]
+        # nodes = [1, 2, 3, 4, 5]
+        # edges = [(1, 2, 3), (1, 3, 10), (2, 3, 5), (2, 4, 7), (2, 5, 3), (3, 4, 1),(3, 5, 1), (4, 5, 4)]
     nodes = [1,2,3,4,5,6,7,8,9]
     edges = [(1,2,2),(1,4,3),(2,3,9),(2,5,2),(3,6,1),(4,5,4),(4,7,5),(5,6,14),(5,8,6),(6,9,2),
                 (7,8,10),(8,9,7)]
+
+    nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    edges = [(1, 2, 5), (2, 3, 7), (3,4,9),(1,5,61), (2,6,1), (3,7,4),(4,8,89),(5,6,3),(6,7,37),(7,8,44),
+             (5,9,2),(5,10,91),(6,11,96),(8,12,54), (9,10,1), (10,11,1), (11,12,6),
+             (9,13,33), (10,14,6) , (11,15,57), (12,16,103),
+             (13,14,17),(14,15,8), (15,16,99)]
+
     for num in nodes:
         g.add_simple_node(num)
 
     for ed in edges:
         g.add_simple_edge(ed[0],ed[1],ed[2])
         g.add_simple_edge(ed[1], ed[0], ed[2])
+        # mat_len = 4
+        # g = simple_graph()
+        # for i in range(len(matrix)):
+        #     for j in range(len(matrix[0])):
+        #         g.add_simple_node((mat_len*i)+j+1)
+        #
+        # for i in range(len(matrix)):
+        #     for j in range(len(matrix[0])):
+        #         # try for all four neighbors in order: right, bottom, left, top
+        #         # note here the order in which the neighbors are stored is imp
+        #         if j + 1 < len(matrix[0]):
+        #             g.add_simple_edge(((mat_len*i)+j+1), ((mat_len*i)+ j + 1+1), matrix[i][j])
+        #         if i + 1 < len(matrix):
+        #             g.add_simple_edge(((mat_len*i)+j+1), (i + 1, j), matrix[i][j])
+        #         if j - 1 >= 0:
+        #             grph.add_edge(((mat_len*i)+j+1), (i, j - 1), matrix[i][j])
+        #         if i - 1 >= 0:
+        #             grph.add_edge(((mat_len*i)+j+1), (i - 1, j), matrix[i][j])
+
 
     return g
 
@@ -28,7 +54,7 @@ def test_dijkstra():
     # for nodes in graph.vert_list:
     #     print(nodes,":",graph.vert_list[nodes])
 
-    run_dijkstra(graph, 1)
+    run_dijkstra(graph, 11)
 
 
 def update(source_node, dist, parent, min_heap):
@@ -85,8 +111,9 @@ def run_dijkstra(graph, source_node):
             update(simple_min_node, dist, parent, distanes_heap)
             processed_vertices.add(min_node)
 
-    print(dist)
-    print(parent)
+    for i in range(len(graph.vert_list)):
+        print(str(i+1)," ",dist[i+1]," ",parent[i+1])
+
 
 
 
