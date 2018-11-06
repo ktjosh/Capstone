52c3f4997928d8c9fd4b83531689391d41fe5c2d
ktjosh <ksj4205@rit.edu>
2018-11-05 22:13:31 -0500

    Read and display image

--- Image_segmentation.py
+++ Image_segmentation.py
@@ -1,2 +1,24 @@
 import cv2
 import numpy as np
+image_URL = "C:\\Users\\ktjos\\Desktop\\Lumpy.jpg"
+
+def run_image_segmentation(image):
+    pass
+
+
+def get_image():
+    img = cv2.imread(image_URL)
+    height_img = img.shape[0]
+    width_img = img.shape[1]
+
+    cv2.namedWindow('my_window',cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('my_window', width_img, height_img)
+
+    cv2.imshow('my_window', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    # run_image_segmentation()
+    get_image()
